a917c1f815c7fda59fdd9ee962345d8874c741c1
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2023-11-18 10:44:48 +0000

    conversion of data types

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "harsh"
+console.log(typeof score)
+console.log(typeof(score)) //we can also write line number 7 as this
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN ()
+// true => 1; flase => 0
+
+console.log("NEXT PART")
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "harsh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => flase
+// "" => flase
+// "harsh" => true
+
+console.log("Next part")
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
