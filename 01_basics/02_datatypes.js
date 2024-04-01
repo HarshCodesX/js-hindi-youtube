e5b89123e74c10f8dc65564b45c7d8707401a153
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-04-01 10:18:25 +0000

    name changed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name = "Harsh"
+let age = 3
+let isLoggedIn = false
+
+//PRIMITIVE DATA TYPES
+//number = 2 to the power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined = it means that value has not been assigned yet
+//symbol = whenever we want to talk about unique, then we talk about symbols
+
+//not primitive data type
+//object
+
+console.log(typeof null) //output -> object 
+console.log(typeof undefined) //output -> undefined
