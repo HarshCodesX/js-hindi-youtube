900911c801d00e9cd1bc18aeabcc3677442a120e
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-04-01 10:55:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3); //we are using node js, not browser
+
+let name = "harsh"; //this is string datatype
+let age = 19; //this is integer
+let isLoggedIn = false; //this is boolean
+let state; //undefined
+
+/*
+    PRIMITIVE
+
+    number/integers
+    bigInt
+    string = ""
+    boolean = true/false
+    null = In js null is also a datatype or we can say it is a standalone value
+    undefined = i.e we have not assigned any value, or we can say we have declared a variable but not assigned any value
+    symbol = used to find uniqueness in different components like id in css
+*/
+
+/*
+    NON-PRIMITIVE
+
+    objects
+*/
+
+console.log(typeof "harsh");
+console.log(typeof age);
+console.log(typeof null);    // its output will be object
+console.log(typeof undefined); //its output will be undefined
