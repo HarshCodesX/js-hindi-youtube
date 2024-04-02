554e438792166f716708044de227d53cb35c1694
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-04-02 14:13:59 +0000

    comparison file

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,50 @@
+// console.log(2>1);
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1) // while comparing any 2 values or variables, make sure that their data type is also same
+// console.log("02">1) // while comparing any 2 values or variables, make sure that their data type is also same
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+
+console.log("comparison b/w null and 0");
+console.log(null>0)
+console.log(null==0);
+console.log(null<0);
+console.log(null<=0)
+console.log(null>=0)
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+
+console.log("comparison b/w undefined and 0");
+console.log(undefined>0)
+console.log(undefined==0);
+console.log(undefined<0);
+console.log(undefined<=0)
+console.log(undefined>=0)
+
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+
+console.log("comparison b/w undefined and null");
+console.log(undefined>null)
+console.log(undefined==null);
+console.log(undefined<null);
+console.log(undefined<=null)
+console.log(undefined>=null)
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+
+console.log("strict check ===");
+//when we compare "2" == 2, then it converts string into a number and then return true, 
+// but when we do "2" === 2, it will also check the data type of both the value/variables and then return false as one is string and another is number/integer
