20210455a5b3d06cc35024e05fa3f272a85534ad
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-05-01 19:16:05 +0000

    few lines added

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -10,9 +10,11 @@ console.log(`my name is ${name} and my repo count is ${repoCount}`); //this meth
 
 
 //new way to declare string
-const gameName = new String('harshhhc');
+const gameName = new String('harshh-hc');
 console.log(gameName);
 console.log(typeof(gameName));
+
+//When you create a string using the String constructor function, it returns a string object rather than a primitive string value. This means that the value stored in gameName is not just a primitive string, but an instance of the String object.
 
 console.log(gameName[0]);
 console.log(gameName.__proto__); // outputs: {}, provides the prototype and there are different methods in prototype and some are
@@ -21,4 +23,14 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('r'));
 
-//When you create a string using the String constructor function, it returns a string object rather than a primitive string value. This means that the value stored in gameName is not just a primitive string, but an instance of the String object.
+// few more methods
+const newString = gameName.substring(0,4); //can not enter/pass negative value in substring function
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4); //can pass negative value in slice function
+console.log(anotherString);
+
+//few more
+const newStringOne = "    harsh    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
