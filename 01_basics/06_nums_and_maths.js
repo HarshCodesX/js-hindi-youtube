fdc28e60c39092c147f08fcbc85a50226841dc39
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-05-02 10:33:35 +0000

    made some changes

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,5 @@
+const score = 400;
+
+//now we can explicitly define that the variable we are deeclaring has to be a number using new keyword and Number object as in js we get different objects such as String, Number, etc
+const balance = new Number(100);
+console.log(balance);
