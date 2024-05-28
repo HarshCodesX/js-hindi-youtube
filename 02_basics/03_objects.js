166fe9874dd77350c6b966d154762c5ad847e84e
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-05-28 19:55:33 +0000

    part one done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -25,20 +25,35 @@ const jsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 };
 
-console.log(jsUser.email);
-console.log(jsUser["email"]);
-console.log(jsUser["full name"]);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
 
 // console.log(jsUser.mySym); //but it is not working as a symbol
 
 //correct syntax
-console.log(jsUser[mySym]);
+// console.log(jsUser[mySym]);
 
 //how to change value 
 jsUser.email = "harsh@chatgpt.com";
-console.log(jsUser);
+// console.log(jsUser);
 
 //if we want that any value dont get changed
-Object.freeze(jsUser);
+// Object.freeze(jsUser); //if it is not commented, then it was giving error while makeing changes in object
 jsUser.email = "harshdaskhb055@gmail.com";
-console.log(jsUser);
+// console.log(jsUser);
+
+//lets add a function inside it
+jsUser.greeting = function(){
+    console.log("hello js user");
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.name}`);
+}
+
+console.log(jsUser.greeting);
+console.log(jsUser.greeting());
+
+console.log(jsUser.greetingTwo);
+console.log(jsUser.greetingTwo());
