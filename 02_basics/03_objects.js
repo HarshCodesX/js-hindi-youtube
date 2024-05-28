96bb7858540c5a23e556ccbf6c38139102d0fb56
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-05-28 17:11:21 +0000

    written some code

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,44 @@
+// an object is a collection of properties, and a property is an association between a name and a value.
+//objects can be defined in 2 ways: i) using Literals ii) using constructor
+
+//singleton (object.create)
+//whenever objects are created using constructor, then object created is singleton (i.e one of its own kind)
+//but when you create object using literals, then it is not singleton, multiple instance of that object are created
+
+const mySym = Symbol("key1");
+
+//object literals
+
+const jsUser = {
+    name: "harsh",
+    "full name": "harsh gupta",
+
+
+    // mySym: "mykey1", //not proper syntax to include symbol in object
+    [mySym]: "myKey1",
+
+
+    age: 21,
+    location: "haryana",
+    email: "harsh@microsoft.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+};
+
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+
+// console.log(jsUser.mySym); //but it is not working as a symbol
+
+//correct syntax
+console.log(jsUser[mySym]);
+
+//how to change value 
+jsUser.email = "harsh@chatgpt.com";
+console.log(jsUser);
+
+//if we want that any value dont get changed
+Object.freeze(jsUser);
+jsUser.email = "harshdaskhb055@gmail.com";
+console.log(jsUser);
