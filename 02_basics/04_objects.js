7abb9bab4adab589b37dff4c86dadb35e715a0b9
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-05-29 09:55:17 +0000

    more lines

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -32,5 +32,37 @@ const obj2 = {
 };
 
 // const obj3 = {obj1, obj2}; //will give same problem as it give while combining array, so avoid this method
-const obj3 = Object.assign({}, obj1, obj2);
-console.log(obj3);
+
+// const obj3 = Object.assign({}, obj1, obj2); 
+//When using Object.assign() to combine objects, the {} argument (an empty object) serves a specific and important purpose. 
+//It acts as the target object to which properties from the source objects (in this case, obj1 and obj2) will be copied. 
+//This ensures that the original objects (obj1 and obj2) remain unchanged. If we dont use {}, obj 1 or obj2 might get changed
+
+// console.log(obj1);
+// console.log(obj2);
+// console.log(obj3);
+
+//another method to combine them: using spread operator
+const returnedObj = {...obj1, ...obj2};
+// console.log(returnedObj);
+
+
+//************************ANOTHER METHOD*********************** */
+const users = [
+    {
+        id: 1,
+        email: "harsh@email"
+    },
+
+    {
+        id: 1,
+        email: "harsh@gmail"
+    },
+
+    {
+        id: 1,
+        email: "harsh@ybl"
+    },
+]
+
+users[1].email
