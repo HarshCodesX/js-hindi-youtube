cde1b80f862a7269eb7b35f95c25a3d289be4a66
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-05-30 06:30:13 +0000

    added function file

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+    console.log("h");
+    console.log("a");
+    console.log("r");
+    console.log("s");
+    console.log("h");
+}
+
+// console.log(sayMyName); //this is known as reference of function
+
+sayMyName(); //execution of function
+console.log(sayMyName());
+
+//line 11 and 12 are both execution of function, only difference is that sayMyName() calls the function and logs the value,
+//but console.log(sayMyName()) logs the return value of the function and this function does not return any value explicitly
+//therefore returning a default value of undefined
