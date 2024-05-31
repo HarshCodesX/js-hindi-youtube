96bc4cd5d4892ea92da1149f23d0ef4f8f9ff211
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-05-31 07:27:14 +0000

    made some changes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,11 +6,45 @@ function sayMyName(){
     console.log("h");
 }
 
-console.log(sayMyName); //this is known as reference of function
+// console.log(sayMyName); //this is known as reference of function
 
-sayMyName(); //execution of function
-console.log(sayMyName());
+// sayMyName(); //execution of function
+// console.log(sayMyName());
 
 //line 11 and 12 are both execution of function, only difference is that sayMyName() calls the function and logs the value,
 //but console.log(sayMyName()) logs the return value of the function and this function does not return any value explicitly
 //therefore returning a default value of undefined
+
+//**********ADDING 2 NUMBERS USING FUNCTIONS************ */
+
+function addTwoNumbers(number1, number2){ 
+    console.log(number1 + number2);
+}
+//while defining a function, the variables passed are called parameters
+
+// addTwoNumbers(3, 4); //what if user enter numbers in the form of strings
+// addTwoNumbers(3, "4"); //type conversion occurs
+// addTwoNumbers(3, "a");
+// addTwoNumbers(3, null);
+// addTwoNumbers(3, undefined); //while calling function, the value we pass are known as arguments
+
+//lets store the output value of this function inside a variable
+const result = addTwoNumbers(3,5);
+console.log("Result: ", result);
+
+//the 8 is not known as the return value of the function addTwoNumbers.
+//The 8 is the output of the console.log statement inside the function.
+//The function itself does not return a value; it only logs the result to the console.
+//The return value of the function is undefined by default because there is no return statement.
+
+
+// ************Now make a function that actually returns a value**************
+
+function subtractTwoNumbers(number1, number2){ 
+    let output = number1 - number2;
+    return output;
+    console.log("harsh"); //wont get printed as it is below return statement
+}
+
+const return_result = subtractTwoNumbers(10, 5);
+console.log(return_result);
