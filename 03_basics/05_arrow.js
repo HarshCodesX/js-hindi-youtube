a462ddc464a870466c9a11cbe55b0f1f6af62e59
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-06-05 09:09:19 +0000

    more to it

--- 03_basics/05_arrow.js
+++ 03_basics/05_arrow.js
@@ -3,8 +3,11 @@ const user = {
     price: 999,
     welcomeMessage: function(){
         console.log(`${this.username}, welcome to website`);
+        console.log(this);
     }
 }
 user.welcomeMessage();
 user.username = "sam";
 user.welcomeMessage();
+
+console.log(this)
