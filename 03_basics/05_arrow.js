ff35ed6ef9e5267975156e9b2a210d7081ee9caa
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-06-05 10:42:38 +0000

    topic done

--- 03_basics/05_arrow.js
+++ 03_basics/05_arrow.js
@@ -6,8 +6,48 @@ const user = {
         console.log(this);
     }
 }
-user.welcomeMessage();
+// user.welcomeMessage();
 user.username = "sam";
-user.welcomeMessage();
+// user.welcomeMessage();
 
-console.log(this)
+// console.log(this)
+
+function chai(){
+    let username = "harsh";
+    console.log(this.username);
+}
+// chai() //concept of context only work inside objects
+
+// const drinks = function(){
+//     let drinkName = "chai";
+//     console.log(this.drinkName);
+// }
+
+// drinks(); // same result if we declare and define function this way and call it
+
+// ******************declaring function with the help of arrow function************
+
+const drinks = () => {
+    let drinkName = "fanta";
+    console.log(this);
+}
+
+// drinks();
+
+// *************more about arrow function***********
+() => {}
+
+//we can hold arrow function inside a variable like this:
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2; //this is explicit return
+// }
+
+//concept of implicit return
+// const addTwo = (num1, num2) => num1 + num2;
+// const addTwo = (num1, num2) => (num1 + num2); //we can write the above line like this also
+
+//what if we want to return an object
+const addTwo = (num1, num2) => ({username: "harsh"});
+
+console.log(addTwo(3,4)); //while calling line number 51, here arguments passed are of no use as we are printing object which contains string
