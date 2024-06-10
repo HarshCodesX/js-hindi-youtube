fd2b37404c2a2766164f17efe15105c9cfba553b
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-06-10 03:41:30 +0000

    control flow started

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+//if
+const isUserLoggedIn = true;
+const temperature = 41;
+
+// if(temperature === 40){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("execute");
+// <, >, <=, >=, ==,!=, ===, !==
+
+// *********************************************NEW ONE*************************************************
+const score = 200;
+if(score > 100){
+    const power = "fly";
+    console.log(`User power: ${power}`);
+}
