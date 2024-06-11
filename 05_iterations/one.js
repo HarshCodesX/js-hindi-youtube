54d197ac9212918ae7bfed9d68e31a62f1b860d1
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-06-11 08:37:54 +0000

    for loop done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -11,9 +11,35 @@ for (let i = 0; i <= 10; i++) {
 //******************NEW ONE************************* */
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`outer loop value: ${i}`);
+    // console.log(`outer loop value: ${i}`);
     for (let j = 1; j <= 10; j++) {
         // console.log(`inner loop value: ${j} and outer loop ${i}`);
-        console.log(i + "*" + j + " = " + i*j);
+        // console.log(i + "*" + j + " = " + i*j);
     }    
+}
+
+//******************NEW ONE********************************* */
+let myArray = ["flash", "batman", "superman"];
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+// ****************************NEW ONE (ABOUT BREAK) ***********************
+for (let index = 1; index < 20; index++) {
+    if (index == 5) {
+        // console.log("detected 5");
+        break;
+    }
+    // console.log(`value of i is ${index}`)    
+}
+
+// *********************NEW ONE (ABOUT CONTINUE)*****************************
+for (let index = 1; index < 20; index++) {
+    if (index == 5) {
+        console.log("detected 5");
+        continue;
+    }
+    console.log(`value of i is ${index}`)    
 }
