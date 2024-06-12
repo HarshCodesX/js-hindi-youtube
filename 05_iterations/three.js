9ad19ab1c0d2cbba648251e5359f21a0ebdcaf3d
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-06-12 08:21:05 +0000

    added three.js

---
+++ 05_iterations/three.js
@@ -0,0 +1,53 @@
+// for of 
+
+// ["", "", ""] array of strings
+// [{}, {}, {}] array of objects
+
+const arr = [1, 2, 3, 4, 5];
+for (const num of arr) {
+    // console.log(num);
+}
+
+// ********************************applying for of loop on strings***************************
+const greetings = "Hello World";
+for (const greet of greetings) {
+    if(greet == " "){
+        // console.log("space detected");
+        // break;
+        continue;
+    }
+    // console.log(greet);
+}
+
+// **************************MAPS****************************
+
+//Maps (map is a object in itself)
+// Map is an object that holds key valle pairs and remember the orignal insertion of the keys (i.e remember the order in which key value pair was inserted)
+// It is unique which means there is no duplicate value inside a map
+
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("FR", "France");
+map.set("IN", "India"); //we have entered india again, but this will not be consider inside map as it will be only entered once
+
+// console.log(map);
+
+for (const key of map) {
+    console.log(key); //it will provide us array of key value pair, each key value pair will make a separate array
+}
+
+// to destructure that array, we can use this syntax
+for (const [key, value] of map) {
+    console.log(key, ":-", value)
+}
+
+// **************for of loop on object*********************
+const myObject = {
+    "game1" : "NFS",
+    "game2" : "spiderman"
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ":-", value); //will throw error that myObject is not iterable as we cant iterate it with for of loop, there are atoher methods
+}
