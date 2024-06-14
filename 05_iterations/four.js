bb75b9c46c60b2f75a5c2eef5e081e08a8bb002b
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-06-14 09:13:13 +0000

    last change

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -17,6 +17,17 @@ for (const key in myObject) {
 // *************************APPLYING forin LOOP ON ARRAY*******************************
 const programming = ["js", "rb", "py", "java", "cpp"];
 for (const key in programming) {
-    // console.log(key);
+    // console.log(key); //if we did this in for of loop, then it directly prints value of the array, but here it is printing key only
     console.log(programming[key]);
+}
+
+//**************************** APPLYING "FOR IN" LOOP ON MAP **********************************/
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("FR", "France");
+map.set("IN", "India");
+
+for (const key in map) {
+    // console.log(key); //map is not iterateable, and if anything which is not iterateable, we cant write it inside any loop like this directly
 }
