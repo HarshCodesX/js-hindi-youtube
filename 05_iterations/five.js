6359ae7efb0a1126a812d49374052a105775be8d
HarshCodesX <147278925+HarshCodesX@users.noreply.github.com>
2024-06-14 09:44:56 +0000

    done some work

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,4 +1,27 @@
 const coding = ["js", "ruby", "java", "python", "cpp"];
+
 coding.forEach(function (val){
-    console.log(val);
+    // console.log(val);
 })
+
+coding.forEach( (item) => {
+    // console.log(item);
+} )
+
+// ******************* WE CAN ALSO PASS FUNCTION*********************
+function printMe(item){
+    // console.log(item);
+}
+coding.forEach(printMe); //here reference of the function has to be passed like this "printMe" instead of calling the function liek this "printMe()""
+
+// *****************NEW ONE*********************
+coding.forEach((item, index, arr) => {
+    // console.log(item, index, arr);
+})
+
+// **********************NEW ONE*****************************
+const myCoding = [
+    {
+        languageName
+    }
+]
